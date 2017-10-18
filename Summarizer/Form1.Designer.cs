96c3e0fd99148e6aa25a51344c3bf32a6f0fb9bd
Kyle <kcdodaro@gmail.com>
2017-10-18 14:53:49 -0400

    Initial commit.

--- Summarizer/Form1.Designer.cs
+++ Summarizer/Form1.Designer.cs
@@ -28,12 +28,111 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblAmountSentences = new System.Windows.Forms.Label();
+            this.grpSentOrPerc = new System.Windows.Forms.GroupBox();
+            this.radSentences = new System.Windows.Forms.RadioButton();
+            this.radPercentage = new System.Windows.Forms.RadioButton();
+            this.rtxtInput = new System.Windows.Forms.RichTextBox();
+            this.lstOutput = new System.Windows.Forms.ListBox();
+            this.btnSummarize = new System.Windows.Forms.Button();
+            this.grpSentOrPerc.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblAmountSentences
+            // 
+            this.lblAmountSentences.AutoSize = true;
+            this.lblAmountSentences.Location = new System.Drawing.Point(451, 9);
+            this.lblAmountSentences.Name = "lblAmountSentences";
+            this.lblAmountSentences.Size = new System.Drawing.Size(113, 13);
+            this.lblAmountSentences.TabIndex = 0;
+            this.lblAmountSentences.Text = "Amount of sentences: ";
+            // 
+            // grpSentOrPerc
+            // 
+            this.grpSentOrPerc.Controls.Add(this.radPercentage);
+            this.grpSentOrPerc.Controls.Add(this.radSentences);
+            this.grpSentOrPerc.Location = new System.Drawing.Point(454, 25);
+            this.grpSentOrPerc.Name = "grpSentOrPerc";
+            this.grpSentOrPerc.Size = new System.Drawing.Size(110, 69);
+            this.grpSentOrPerc.TabIndex = 1;
+            this.grpSentOrPerc.TabStop = false;
+            this.grpSentOrPerc.Text = "Cut down by";
+            // 
+            // radSentences
+            // 
+            this.radSentences.AutoSize = true;
+            this.radSentences.Location = new System.Drawing.Point(6, 19);
+            this.radSentences.Name = "radSentences";
+            this.radSentences.Size = new System.Drawing.Size(76, 17);
+            this.radSentences.TabIndex = 0;
+            this.radSentences.TabStop = true;
+            this.radSentences.Text = "Sentences";
+            this.radSentences.UseVisualStyleBackColor = true;
+            // 
+            // radPercentage
+            // 
+            this.radPercentage.AutoSize = true;
+            this.radPercentage.Location = new System.Drawing.Point(6, 42);
+            this.radPercentage.Name = "radPercentage";
+            this.radPercentage.Size = new System.Drawing.Size(80, 17);
+            this.radPercentage.TabIndex = 1;
+            this.radPercentage.TabStop = true;
+            this.radPercentage.Text = "Percentage";
+            this.radPercentage.UseVisualStyleBackColor = true;
+            // 
+            // rtxtInput
+            // 
+            this.rtxtInput.Location = new System.Drawing.Point(12, 12);
+            this.rtxtInput.Name = "rtxtInput";
+            this.rtxtInput.Size = new System.Drawing.Size(433, 327);
+            this.rtxtInput.TabIndex = 2;
+            this.rtxtInput.Text = "";
+            // 
+            // lstOutput
+            // 
+            this.lstOutput.FormattingEnabled = true;
+            this.lstOutput.Location = new System.Drawing.Point(12, 345);
+            this.lstOutput.Name = "lstOutput";
+            this.lstOutput.Size = new System.Drawing.Size(433, 186);
+            this.lstOutput.TabIndex = 3;
+            // 
+            // btnSummarize
+            // 
+            this.btnSummarize.Location = new System.Drawing.Point(454, 100);
+            this.btnSummarize.Name = "btnSummarize";
+            this.btnSummarize.Size = new System.Drawing.Size(110, 23);
+            this.btnSummarize.TabIndex = 4;
+            this.btnSummarize.Text = "Summarize";
+            this.btnSummarize.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(576, 542);
+            this.Controls.Add(this.btnSummarize);
+            this.Controls.Add(this.lstOutput);
+            this.Controls.Add(this.rtxtInput);
+            this.Controls.Add(this.grpSentOrPerc);
+            this.Controls.Add(this.lblAmountSentences);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.grpSentOrPerc.ResumeLayout(false);
+            this.grpSentOrPerc.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblAmountSentences;
+        private System.Windows.Forms.GroupBox grpSentOrPerc;
+        private System.Windows.Forms.RadioButton radPercentage;
+        private System.Windows.Forms.RadioButton radSentences;
+        private System.Windows.Forms.RichTextBox rtxtInput;
+        private System.Windows.Forms.ListBox lstOutput;
+        private System.Windows.Forms.Button btnSummarize;
     }
 }
 
---
+++ Summarizer/Sentence.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Summarizer
+{
+    class Sentence
+    {
+        public int intID { get; set; }
+        public int intAmountWords { get; set; }
+        public int intScore { get; set; }
+        public string strSentence { get; set; }
+
+        public Sentence(int id, int amountwords, int score, string sentence)
+        {
+            try
+            {
+                intID = id;
+                intAmountWords = amountwords;
+                intScore = score;
+            }
+            catch
+            {
+                Console.WriteLine("Invalid data type in Sentence Class.");
+            }
+            strSentence = sentence;
+        }
+    }
+}
